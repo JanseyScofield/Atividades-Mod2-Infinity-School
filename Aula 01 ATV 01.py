52041ff1f75fef08d0c024334b037a7aa539bd05
Jansey Scofield <163556555+JanseyScofield@users.noreply.github.com>
2024-05-04 11:59:41 -0300

    Aula 01 ATV 01.py

--- Aula 01 ATV 01.py
+++ Aula 01 ATV 01.py
@@ -1,13 +1,20 @@
-idade = int(input("Digte sua idade: "))
+while True:
+    try:
+      idade = (int(input(f"Digite número: ")))
+    except Exception:
+        print("Número inválida! Digite novamente")
+    else:
+        if idade < 12:
+            resp = "criança"
+        elif idade >= 12 and idade <=17:
+            resp = "adolescente"
+        elif idade >=18 and idade <=59:
+            resp = "adulto"
+        else:
+            resp = "idoso"
+        print(f"Você é {resp}.")
+    escolha = input("Quer digitar outra idade? Sim(s) ou Não(n)? ").lower()
+    if escolha == "n":
+        break
 
-if idade < 12:
-    resp = "criança"
-elif idade >= 12 and idade <=17:
-    resp = "adolescente"
-elif idade >=18 and idade <=59:
-    resp = "adulto"
-else:
-   resp = "idoso"
-
-
-print(f"Você é {resp}.")
+print("Fim!")
