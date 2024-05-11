f2b05800bd805eeeac021e5e709316133e738b11
Jansey Scofield <163556555+JanseyScofield@users.noreply.github.com>
2024-05-11 08:06:13 -0600

    Aula 02 ATV 01.py

---
+++ Aula 02 ATV 01.py
@@ -0,0 +1,6 @@
+# Faça a definição de uma lista contendo os números de 1 até 5. Finalmente, utilize o print() para exibir os valoresda lista.
+
+listNumbers = [1,2,3,4,5]
+
+for iCont in listNumbers:
+    print(iCont)
