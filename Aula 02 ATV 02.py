fe5e36880ca4eb527874da04bdbce99ed69643db
Jansey Scofield <163556555+JanseyScofield@users.noreply.github.com>
2024-05-11 08:09:42 -0600

    Aula 02 ATV 02.py

---
+++ Aula 02 ATV 02.py
@@ -0,0 +1,6 @@
+# Faça a definição de uma lista contendo as vogais.Finalmente, utilize o print() para exibir os valores da lista.
+
+listVogals = ['a', 'e', 'i', 'o', 'u']
+
+for iCont in listVogals:
+    print(iCont)
