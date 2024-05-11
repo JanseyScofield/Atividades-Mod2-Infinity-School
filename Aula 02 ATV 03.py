10dcab9bdf2906b657a041b0287ea98d584fed7f
Jansey Scofield <163556555+JanseyScofield@users.noreply.github.com>
2024-05-11 08:25:32 -0600

    Aula 02 ATV 03.py

---
+++ Aula 02 ATV 03.py
@@ -0,0 +1,5 @@
+# Defina uma lista com 5 itens que tenha, pelo menos, 3 classes diferentes. Utilize o print() para exibir o terceiro elemento dessa lista.
+
+list = ['a', 12, 13.5, True, False ]
+
+print(list[2])
