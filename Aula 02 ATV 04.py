f94d7f0dba46d6f5758c60539a4781bac9bd8a3c
Jansey Scofield <163556555+JanseyScofield@users.noreply.github.com>
2024-05-11 09:01:29 -0600

    Aula 02 ATV 04.py

---
+++ Aula 02 ATV 04.py
@@ -0,0 +1,6 @@
+# Crie uma tupla para representar as informações de três palestrantes, cada uma contendo o nome, o tema da palestra e a instituição à qual estão vinculados. 
+# Exiba na tela as informações do terceiro palestrante, incluindo nome, tema da palestra e instituição.
+
+palestrantes = (("Renan", "Python", "Inifinity School"), ("Elon Musk", "Rocket", "Tesla"), ("Pablo Marçal", "Natação", "Empresa X"))
+
+print(palestrantes[2])
