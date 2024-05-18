6f5a6403d24e4cca8f8c4e7d3459d12133baf13b
Jansey Scofield <163556555+JanseyScofield@users.noreply.github.com>
2024-05-18 12:17:00 -0300

    Aula 03 ATV 04.py

--- Aula 03 ATV 04.py
+++ Aula 03 ATV 04.py
@@ -1,6 +1,6 @@
-# Escreva um programa que EXIBA um dicionário contendo informações de pessoas (nome, idade) e exiba essas informações.
+# Escreva um programa que EXIBA um dicionário contendo informações de pessoas (nome, idade) e exiba essas informações separadamente.
 nome = input("Digite seu nome: ")
-idade = int(input("Digite sua idade :"))
+idade = int(input("Digite sua idade: "))
 
 dados = {
     "nome" : nome,
@@ -8,3 +8,6 @@ dados = {
 }
 
 print(dados)
+
+for chave , valor in dados.items():
+    print(f"{chave} : {valor}")
