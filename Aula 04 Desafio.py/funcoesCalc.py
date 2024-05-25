db7016a14dbcf187c0dfd8d3e0ca9c503193b3bf
Jansey Scofield <163556555+JanseyScofield@users.noreply.github.com>
2024-05-25 12:58:35 -0300

    funcoesCalc.py

---
+++ Aula 04 Desafio.py/funcoesCalc.py
@@ -0,0 +1,11 @@
+def somar(n1 : float, n2 : float) -> float:
+    return n1 + n2
+
+def subtrair(n1 : float, n2 : float) -> float:
+    return n1 - n2
+
+def multiplicar(n1 : float, n2 : float) -> float:
+    return n1 * n2
+
+def dividir(n1 : float, n2 : float) -> float:
+    return n1 / n2
