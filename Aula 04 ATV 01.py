97bbc8361ea298009ac9378784f899b7bfb12d8d
Jansey Scofield <163556555+JanseyScofield@users.noreply.github.com>
2024-05-25 11:32:45 -0300

    Aula 04 ATV 01.py

---
+++ Aula 04 ATV 01.py
@@ -0,0 +1,7 @@
+# Crie uma função que receba um nome e imprima uma saudação personalizada.
+
+def saudarPersonalizado(nome : str):
+    """Essa função recebe um nome e imprime uma saudação personalizada."""
+    print(f"Bem vindo, {nome}!")
+
+saudarPersonalizado("Jansey")
