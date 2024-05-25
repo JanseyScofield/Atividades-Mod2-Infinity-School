acb45ab44b84ed0fbe7528419f731d6f91cd3344
Jansey Scofield <163556555+JanseyScofield@users.noreply.github.com>
2024-05-25 11:51:07 -0300

    Aula 04 ATV 03.py

---
+++ Aula 04 ATV 03.py
@@ -0,0 +1,7 @@
+# Crie uma função que receba dois números e retorne a soma deles.
+
+def somar(n1 : float, n2 : float):
+    """Essa função recebe dois números reais e retorna a soma deles"""
+    return n1 + n2
+
+print(somar(14,12.3))
