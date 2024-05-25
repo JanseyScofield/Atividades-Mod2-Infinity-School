bce251eb65b96080c764e176287d63436b38dfbe
Jansey Scofield <163556555+JanseyScofield@users.noreply.github.com>
2024-05-25 11:59:35 -0300

    Aula 04 ATV 05.py

---
+++ Aula 04 ATV 05.py
@@ -0,0 +1,8 @@
+# Crie uma função que receba dois números e retorne a multiplicação deles.
+
+def multiplicar(n1 : float, n2 : float) -> float:
+    """A função recebe dois número reais e retorna o resultado da multiplicação deles."""
+
+    return n1 * n2
+
+print(multiplicar(3.2, 4))
