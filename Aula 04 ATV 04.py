6628b41e0f01ad312fe21a8f4432d261023ed942
Jansey Scofield <163556555+JanseyScofield@users.noreply.github.com>
2024-05-25 11:54:02 -0300

    Aula 04 ATV 04.py

---
+++ Aula 04 ATV 04.py
@@ -0,0 +1,7 @@
+# Crie uma função que receba dois números e retorne a subtração do primeiro pelo segundo.
+
+def subtrair(n1 : float, n2 : float):
+    """Recebe dois números reais e retorna o resultado da subtração do primero pelo segundo"""
+    return n1 - n2
+
+print(subtrair(14.2, 55))
